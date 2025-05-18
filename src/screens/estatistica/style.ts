0af2ccc3f028c795d74cf13feaeaab2077040542
Luiz Filipe <filipe.oli2016@gmail.com>
2025-05-18 16:06:21 -0300

    começando estilização
    dando inicio a estilização

--- src/screens/estatistica/style.ts
+++ src/screens/estatistica/style.ts
@@ -1,15 +1,22 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor: 'black', 
-        flex: 1, 
-        alignItems: 'center', 
-        justifyContent: 'center'
-    },
-    text:{
-        color: 'white'
-    }
+  container: {
+    backgroundColor: 'black',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: 'white'
+  },
+  nome: {
+    fontSize: 22,
+    color: '#00ffcc',
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center'
+  }
 });
 
 export default styles;
