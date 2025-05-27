f2dce604fe86a351adbc964fa905177cb7d74f8c
Maycon Vinicius <mayconvbatista84@gmail.com>
2025-05-27 13:27:29 -0300

    Config env

--- src/config/FirebaseConfig.ts
+++ src/config/FirebaseConfig.ts
@@ -3,15 +3,22 @@ import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
-
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJdquG8UpWV7J_S2rZzpJbr9faraIYWhc",
-  authDomain: "trainingup-866ec.firebaseapp.com",
-  projectId: "trainingup-866ec",
-  storageBucket: "trainingup-866ec.firebasestorage.app",
-  messagingSenderId: "86788944753",
-  appId: "1:86788944753:web:9bcca37db4562a7d757cd0"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
